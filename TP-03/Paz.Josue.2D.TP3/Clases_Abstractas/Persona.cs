e1779dd072db29c494a7889572b748029f0c22aa
josupaz <54748658+josupaz@users.noreply.github.com>
2020-06-13 08:14:13 -0300

    act
    act fq

---
+++ TP-03/Paz.Josue.2D.TP3/Clases_Abstractas/Persona.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public string Apellido
+        { get { return this.apellido; }
+          set { this.apellido = value; }
+        }
+
+        public int Dni
+        {
+            get { return this.dni; }
+            set { this.dni = value; }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get { return this.nacionalidad; }
+            set { this.nacionalidad = value; }
+        }
+
+        public string StringToDNI
+        {
+            set { this.dni = int.Parse(value); }
+        }
+
+        public Persona()
+        { }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Nacionalidad = nacionalidad;
+        }
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad) :this(nombre,apellido,nacionalidad)
+        {
+            Dni = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            StringToDNI = dni;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nombre: " + Nombre);
+            sb.AppendLine("Apellido: " + Apellido);
+            sb.AppendLine("Nacionalidad: " + Nacionalidad);
+            sb.AppendLine("Dni: " + Dni);
+
+
+            return sb.ToString();
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+
+        }
+
+
+
+    }
+}
